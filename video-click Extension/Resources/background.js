d5d8687c2299dad6b840d5060b607115cd44061b
constantine <parfilokostya0@gmail.com>
2024-07-05 14:21:55 +0300

    Improve url scraper handle

--- video-click Extension/Resources/background.js
+++ video-click Extension/Resources/background.js
@@ -6,28 +6,35 @@ browser.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
         let html = await response.text();
         let fileSrc = getFileSrcFromHtml(html);
 
-        browser.runtime.sendNativeMessage("application.id", fileSrc, function(response) {
-            console.log(response);
-        });
+        if(!fileSrc) sendResponse('error')
+
+        await sendNative(fileSrc, sendResponse);
     }
-    
-//    let port = browser.runtime.connectNative("application.id");
-//    
-//    port.onMessage.addListener(function(message) {
-//        sendResponse({response: message});
-//    });
+
+   if('src' in request) {
+       await sendNative(request.src, sendResponse);
+   }
 });
 
-//let port = browser.runtime.connectNative("com.pk.video-click.Extension");
-//console.log(port);
+async function sendNative(src, sendResponse){
+    let status = await browser.runtime.sendNativeMessage("application.id", src);
+    await sendResponse(status['status']);
+}
+
 
 function getFileSrcFromHtml(html, playerKeyword = "new Playerjs", fileKeyword = "file:") {
+    if(!html.includes(playerKeyword)) return false;
+
     let firstIndex = html.indexOf(playerKeyword);
     let secondIndex = html.indexOf('})', firstIndex);
     let playerObject = (html.slice(firstIndex, secondIndex)).slice(playerKeyword.length) + '})';
 
     let firstFileIndex = playerObject.indexOf(fileKeyword) + fileKeyword.length;
-    let secondFileIndex = playerObject.indexOf(",", firstFileIndex)
-    let fileSrc = playerObject.slice(firstFileIndex, secondFileIndex);
-    return fileSrc.slice(0, fileSrc.length);
+    let secondFileIndex = playerObject.indexOf(",", firstFileIndex);
+    let srcStr =  playerObject.substring(firstFileIndex, secondFileIndex);
+
+    for(let str of srcStr.split('"')){
+        if(str.length > 4) return str;
+    }
+    return false;
 }
--- video-click Extension/Resources/content.js
+++ video-click Extension/Resources/content.js
@@ -1,30 +1,62 @@
 browser.runtime.onMessage.addListener((request) => {
     if(request.select){
+        let videos = document.querySelectorAll('video');
+        checkValidElements(videos);
+
         let iframes = document.querySelectorAll('iframe');
-        for(let i = 0; i < iframes.length; i++){
-            if(iframes[i].src){
-                let button = document.createElement('button');
+        checkValidElements(iframes, true);
+    }
+});
+
+function checkValidElements(elements, itsIframes = false)
+{
+    if(elements && elements.length > 0){
+        elements.forEach(element => {
+            if(element.hasAttribute('src') && containFormats(element.src, itsIframes)){
+                let button = document.createElement('div');
                 button.style.position = 'absolute';
-                button.style.padding = '3px';
+                button.style.padding = '3px 6px';
+                button.style.border = '1px solid black'
                 button.style.borderRadius = '10%';
                 button.style.background = 'white';
                 button.style.cursor = 'pointer';
                 button.style.visibility = 'hidden';
+                button.style.zIndex = '999';
+                button.style.color = 'black';
                 button.innerHTML = 'Save';
 
                 button.onclick = function(){
-                    browser.runtime.sendMessage({ iframeSrc: iframes[i].src }).then((response) => {
-                        console.log(response);
+                    if(button.innerHTML === 'Saved' || button.innerHTML === 'Error') return;
+
+                    let requestData = itsIframes ? { iframeSrc: element.src} : { src: element.src};
+                        browser.runtime.sendMessage(requestData).then((response) => {
+                        if(response === "saved"){
+                            button.innerHTML = 'Saved';
+                            button.style.color = "rgb(66, 172, 0)";
+                        }
+                        if(response === "error"){
+                            button.innerHTML = 'Error';
+                            button.style.color = "red";
+                        }
                     });
                 }
 
-                iframes[i].insertAdjacentElement('beforebegin', button);
-
-                
-                iframes[i].onmouseover = (event) => {
+                element.insertAdjacentElement('beforebegin', button);
+                element.onmouseover = (event) => {
                     button.style.visibility = 'visible';
                 };
             }
-        }
+        })
     }
-});
+}
+
+function containFormats(src, itsIframes = false){
+    if(itsIframes) return true;
+
+    const formats = ['.mp4', '.webm', '.mov', '.mkv', '.m3u8'];
+    for(format in formats){
+        if(src.includes(format)) return true;
+    }
+
+    return false;
+}
