fa75a3ef4f119b9a9dd2923929b31264ae51a11e
constantine <parfilokostya0@gmail.com>
2024-07-05 14:23:25 +0300

    Change popup style

--- video-click Extension/Resources/popup.js
+++ video-click Extension/Resources/popup.js
@@ -2,9 +2,12 @@ let select = document.getElementById('select');
 
 select.onclick = function(event) {
 
+    if(select.classList.contains('active')) return;
+    
     browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
         browser.tabs.sendMessage(tabs[0].id, {
             select: true,
         });
+        select.classList.add('active');
     });
 };
